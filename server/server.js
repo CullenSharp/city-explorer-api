c0b58973f531d6f07ea30a6c654f3c9ca3effc13
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-21 10:24:02 -0700

    Add some docs

--- server/server.js
+++ server/server.js
@@ -5,13 +5,27 @@ require("dotenv").config();
 const cors = require("cors");
 const weather = require("../data/weather.json");
 
-const app = express();
+const app = express(); //express() creates an Express application
 
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
+//app.get is the route, and expression is the handler, the first argument is the path
+app.get("/", (request, response) => {
+  response.send("Hello, world");
+});
+
+//Routes HTTP GET requests to the specified path with the specified callback functions.
+//(source: Express docs)
 app.get("/weather", (request, response) => {
+  console.log(
+    `
+    You requested weather data for:
+    Location_name: ${request.query.display_name}
+    lat: ${request.query.lat} lon: ${request.query.lon}
+    `
+  );
   const weatherArray = weather.data.map((day) => new Forcast(day));
   response.send(weatherArray);
 });
