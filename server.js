cdcfbcc0636b9060f47a65cfb8a014308acd104d
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-22 12:11:40 -0700

    Fix syntax and move to top level

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Load Environment Variables from .env
+require('dotenv').config();
+
+// Application Dependenices
+const cors = require('cors');
+const express = require('express');
+
+
+// Our Dependencies
+const notFound = require('./components/not-found');
+const getWeatherHandler = require('./components/get-weather-handler');
+const getMovieHandler = require('./components/get-movie-handler');
+
+// Application Setup
+const PORT = process.env.PORT || 3002;
+const app = express(); //express() creates an Express application
+app.use(cors()); // When a request comes in use cors to check it
+
+// Route Definitions
+app.get('/weather', getWeatherHandler);
+app.get('/movies', getMovieHandler);
+app.get('*', notFound);
+
+
+
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
